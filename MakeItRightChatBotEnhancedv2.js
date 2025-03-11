223bcf49193c25cc371cb5203fa049cab619fb1f
samalb <samalb@users.noreply.github.com>
2025-03-11 16:53:35 -0700

    Update MakeItRightChatBotEnhancedv2.js

--- MakeItRightChatBotEnhancedv2.js
+++ MakeItRightChatBotEnhancedv2.js
@@ -15,7 +15,7 @@ function SendMessage() {
         player.SetVar("message", "");
 
         var xhr = new XMLHttpRequest();
-        var url = 'https://api.openai.com/v1/chat/completions'; 
+        var url = 'https://api.openai.com/v1/chat/completions?thread=thread_8tmc23QVDMkhcS9FrTn&assistant=asst_hBvmQQXAqHtkUek96uA83qse&system=I am Alex Morgan, the Head of Marketing at Lumina Tech'; 
 
         xhr.open('POST', url, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
